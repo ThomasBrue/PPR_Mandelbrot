8b3719626eae701b7929665e1b0b2b5c1d749a53
Thomas Stummer <thomas.stummer@grassfish.com>
2022-03-12 19:38:03 +0100

    Finished code

--- PPR_Mandelbrot/PPR_Mandelbrot.cpp
+++ PPR_Mandelbrot/PPR_Mandelbrot.cpp
@@ -1,27 +1,44 @@
+/*
+	Parallel Programming
+	Exercise 2
+	Mandelbrot
+	Thomas Bründl
+	Thomas Stummer
+*/
+
 #include <omp.h>
 #include <stdio.h>
 #include <ostream>
 #include <chrono>
 #include <iostream>
-#include <vector>
-#include "../PPR_Mandelbrot/tga.h"
 #include <tuple>
+#include <vector>
+#include <string>
+#include <thread>
+#include "../PPR_Mandelbrot/tga.h"
 
 using namespace std::chrono;
 using namespace std;
+using namespace tga;
 
-std::tuple<float, float> normalizeToViewRectangle(int pX, int pY, float minX, float minY, float maxX, float maxY, unsigned int w, unsigned int h)
+const int BYTES_PER_PIXEL = 3;
+const int GL_RGB = 0;
+const int BITS_PER_PIXEL = 24;
+const unsigned char MAX_COLOR_CODE = 255;
+
+tuple<float, float> normalizeToViewRectangle(
+	int pX, int pY, float minX, float minY, float maxX, float maxY, 
+	unsigned int w, unsigned int h)
 {
 	float cX = ((maxX - minX) * ((float)pX / (float)w)) + minX;
 	float cY = ((maxY - minY) * ((float)pY / (float)h)) + minY;
 	return { cX, cY };
 }
 
-unsigned char calcPix(int pX, int pY, float minX, float minY, float maxX, float maxY,
-	int maxIterations, unsigned int w, unsigned int h)
+unsigned char calcPix(
+	int pX, int pY, float minX, float minY, float maxX, float maxY,
+	unsigned int maxIterations, unsigned int w, unsigned int h)
 {
-
-	const unsigned char maxColorCode = 255;
 	float cX;
 	float cY;
 
@@ -35,7 +52,7 @@ unsigned char calcPix(int pX, int pY, float minX, float minY, float maxX, float 
 		float y = (zY * zX + zX * zY) + cY;
 		if ((x * x + y * y) > 4)
 		{
-			return (unsigned char)(n * maxColorCode / maxIterations);
+			return (unsigned char)(n * MAX_COLOR_CODE / maxIterations);
 		}
 		zX = x;
 		zY = y;
@@ -43,18 +60,16 @@ unsigned char calcPix(int pX, int pY, float minX, float minY, float maxX, float 
 	return 0;
 }
 
-void parallelSolution(float minX, float minY, float maxX, float maxY,
-	int maxIterations, unsigned int w, unsigned int h)
+void parallelSolution(
+	float minX, float minY, float maxX, float maxY,
+	unsigned int maxIterations, unsigned int w, unsigned int h,
+	int num_threads)
 {
+	vector<unsigned char> v (w * h * BYTES_PER_PIXEL);
 
-	int bytesPerPixel = 3;
-	int GL_RGB = 0;
-	int Bpp = 24;
-
-	std::vector<unsigned char> v(w * h * bytesPerPixel);
-
-	omp_set_num_threads(12);
+	omp_set_num_threads(num_threads);
 	auto startTimeStamp = high_resolution_clock::now();
+
 #pragma omp parallel
 	{
 		for (int y = omp_get_thread_num(); y < h; y += omp_get_num_threads())
@@ -62,7 +77,7 @@ void parallelSolution(float minX, float minY, float maxX, float maxY,
 			for (int x = 0; x < w; x++)
 			{
 				unsigned char color = calcPix(x, y, minX, minY, maxX, maxY, maxIterations, w, h);
-				unsigned int p = (y * w + x) * bytesPerPixel;
+				unsigned int p = (y * w + x) * BYTES_PER_PIXEL;
 				v[p] = color;
 				v[p + 1] = color;
 				v[p + 2] = color;
@@ -72,18 +87,18 @@ void parallelSolution(float minX, float minY, float maxX, float maxY,
 
 	auto stopTimeStamp = high_resolution_clock::now();
 	auto durationMS = duration_cast<microseconds>(stopTimeStamp - startTimeStamp).count() / 1000;
-	std::cout << "Computation_Parallel: " << durationMS << " ms" << std::endl;
+	cout << "Parallel mandelbrot using " << num_threads << " threads took: " << durationMS << " ms." << endl;
 
-	tga::TGAImage image = { v, Bpp, w, h, GL_RGB };
-	tga::saveTGA(image, "image_p.tga");
+	auto fileName = "mandelbrot_parallel_" + to_string(num_threads) + ".tga";
+	tga::TGAImage image = { v, BITS_PER_PIXEL, w, h, GL_RGB };
+	tga::saveTGA(image, &fileName[0]);
 }
 
-void serialSolution(float minX, float minY, float maxX, float maxY,
-	int maxIterations, unsigned int w, unsigned int h)
+void serialSolution(
+	float minX, float minY, float maxX, float maxY,
+	unsigned int maxIterations, unsigned int w, unsigned int h)
 {
-
-	std::vector<unsigned char> v(w * h * 3);
-
+	vector<unsigned char> v (w * h * BYTES_PER_PIXEL);
 	auto startTimeStamp = high_resolution_clock::now();
 
 	for (int y = 0; y < h; y++)
@@ -91,7 +106,7 @@ void serialSolution(float minX, float minY, float maxX, float maxY,
 		for (int x = 0; x < w; x++)
 		{
 			unsigned char color = calcPix(x, y, minX, minY, maxX, maxY, maxIterations, w, h);
-			unsigned int p = (y * w + x) * 3;
+			unsigned int p = (y * w + x) * BYTES_PER_PIXEL;
 			v[p] = color;
 			v[p + 1] = color;
 			v[p + 2] = color;
@@ -100,24 +115,37 @@ void serialSolution(float minX, float minY, float maxX, float maxY,
 
 	auto stopTimeStamp = high_resolution_clock::now();
 	auto durationMS = duration_cast<microseconds>(stopTimeStamp - startTimeStamp).count() / 1000;
-	std::cout << "Computation_Serial: " << durationMS << " ms" << std::endl;
+	cout << "Serial mandelbrot took: " << durationMS << " ms." << endl;
 
-	tga::TGAImage image = { v, 24, w, h, 2 };
-	tga::saveTGA(image, "image_s.tga");
+	TGAImage image = { v, BITS_PER_PIXEL, w, h, GL_RGB };
+	saveTGA(image, "mandelbrot_serial.tga");
 }
 
 int main(int argc, char* argv[])
 {
+	unsigned int maxIterations = 1000;
 	unsigned int w = 1024;
 	unsigned int h = 1024;
 	float minX = -1.5;
 	float minY = -1;
 	float maxX = .5;
 	float maxY = 1;
-	int maxIterations = 1000;
 
-	parallelSolution(minX, minY, maxX, maxY, maxIterations, w, h);
 	serialSolution(minX, minY, maxX, maxY, maxIterations, w, h);
+
+	int coresOnMachine = thread::hardware_concurrency();
+
+	if (coresOnMachine > 1)
+	{
+		for (int cores = 2; cores <= coresOnMachine; cores++)
+		{
+			parallelSolution(minX, minY, maxX, maxY, maxIterations, w, h, cores);
+		}
+	}
+	else
+	{
+		cout << "WARNING: This machine has only one core. No parallel calculation is performed!" << endl;
+	}
 
 	return 0;
 }
